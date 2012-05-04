c36c8118c6a081c3cef55b9112d97820b8e45666
Chris Buckett <chrisbuckett@gmail.com>
2012-05-04 21:23:47 +0100

    Added js and client side code

--- MyApp.dart
+++ MyApp.dart
@@ -1,3 +1,20 @@
+#import('dart:html');
+
 class MyApp {
-  
+
+  MyApp() {
+  }
+
+  void run() {
+    write("Hello World!");
+  }
+
+  void write(String message) {
+    // the HTML library defines a global "document" variable
+    document.query('#status').innerHTML = message;
+  }
+}
+
+void main() {
+  new MyApp().run();
 }
